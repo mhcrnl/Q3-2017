6378a70191db6d9325fadc2dcc8ea6470dbe2d9e
mhcrnl <mhcrnl@gmail.com>
2017-09-12 17:14:40 +0300

    Tue Sep 12 17:14:39 EEST 2017

---
+++ GladeTest/README.md
@@ -0,0 +1,8 @@
+## Compilare:
+```
+gcc -o tut1 tut1.c `pkg-config --cflags --libs gtk+-3.0` -export-dynamic
+```
+
+## Resurse:
+
+[https://prognotes.net/2015/06/gtk-3-c-program-using-glade-3/](https://prognotes.net/2015/06/gtk-3-c-program-using-glade-3/)
---
+++ GladeTest/tut1.c
@@ -0,0 +1,33 @@
+#include <gtk/gtk.h>
+ 
+int main(int argc, char *argv[])
+{
+    GtkBuilder      *builder; 
+    GtkWidget       *window;
+ 
+    gtk_init(&argc, &argv);
+ 
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "tut.glade", NULL);
+ 
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window1"));
+    gtk_builder_connect_signals(builder, NULL);
+ 
+    g_object_unref(builder);
+ 
+    gtk_widget_show(window);                
+    gtk_main();
+ 
+    return 0;
+}
+ 
+// called when window is closed
+void on_window_main_destroy()
+{
+    gtk_main_quit();
+}
+
+void gtk_quit()
+{
+    gtk_main_quit();
+}
